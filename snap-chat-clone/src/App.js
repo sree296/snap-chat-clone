1d15e4ac6ec8d6255e45da50d867562871c07257
sree296 <nivas296@gmail.com>
2021-08-21 20:49:36 +0530

    preview route

--- snap-chat-clone/src/App.js
+++ snap-chat-clone/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture'
+import Preview from './Preview'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
        <h1>Lets Build Snapchat</h1>
-       <WebcamCapture />
+       <Router>
+        <div className="app__body">
+          <Switch>
+          <Route exact path="/preview">
+              <Preview />
+            </Route>
+            <Route exact path="/">
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- snap-chat-clone/src/WebcamCapture.js
+++ snap-chat-clone/src/WebcamCapture.js
@@ -3,6 +3,7 @@ import Webcam from 'react-webcam';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import {useDispatch} from "react-redux";
 import {setCameraImage} from './features/cameraSlice'
+import { useHistory } from 'react-router';
 
 const videoConstraints = {
     width : 250,
@@ -14,12 +15,14 @@ function WebcamCapture(){
     const webcamRef = useRef(null);
     const [image, setImage] = useState(null)
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const capture = useCallback( () => {
         const imageSrc = webcamRef.current.getScreenshot();
         console.log(imageSrc);
-        //setImage(imgageSrc);
+        setImage(imageSrc);
         dispatch(setCameraImage(imageSrc));
+        history.push('/preview');
     }, [webcamRef])
     return (
         <div className="webcamCapture">
