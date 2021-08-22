06304902777147c659ace0369fa554900d7b5532
sree296 <nivas296@gmail.com>
2021-08-22 15:55:48 +0530

    preview icons

--- snap-chat-clone/src/Preview.js
+++ snap-chat-clone/src/Preview.js
@@ -7,6 +7,14 @@ import {resetCameraImage} from './features/cameraSlice'
 
 
 import './Preview.css'
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
+import SendIcon from '@material-ui/icons/Send'
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage)
@@ -24,10 +32,27 @@ function Preview() {
         //history.replace('/');
     };
 
+    const sendPost = () => {
+
+    };
+
     return (
         <div className="preview">
             <CloseIcon onClick={closePreview} className="preview__close"/>
+                <div className="preview__toolbarRight">
+                   <TextFieldsIcon />
+                   <CreateIcon />
+                   <NoteIcon />
+                   <MusicNoteIcon />
+                   <AttachFileIcon />
+                   <CropIcon />
+                   <TimerIcon /> 
+                </div>
             <img src={cameraImage} alt="userImage" />
+            <div onClick={sendPost} className="preview__footer">
+                <h2>Send Now</h2>
+                <SendIcon fontSize="small" className="preview__sendIcon"/>
+            </div>
         </div>
     )
 }
